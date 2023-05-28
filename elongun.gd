5edd9ad26cedfbf42f6d257fa59a453303112c45
dav <usr21p1@gmail.com>
2023-05-27 18:02:55 -0700

    FIx elon

--- elongun.gd
+++ elongun.gd
@@ -12,21 +12,29 @@ var Canparry = true
 var bullet_scene = preload("res://DABULL.tscn")
 
 
-func fire():
+puppet func fire(position, gposition):
 	var bullet = bullet_scene.instance()
-	bullet.direction = $Position2D.global_position - global_position
-	bullet.global_position = $Position2D.global_position
+	bullet.direction = position - gposition
+	bullet.global_position = position
 	get_tree().get_root().add_child(bullet)
+	
+puppet var puppet_flip_h = false
+puppet var puppet_position = Vector2()
 
 func _physics_process(delta):
-	look_at(get_global_mouse_position())
-	if Input.is_action_pressed("ui_right"):
+	if is_network_master():
+		look_at(get_global_mouse_position())
+		if Input.is_action_pressed("ui_right"):
 			velocity.x = min(velocity.x + ACCELERATION, MAX_SPEED)
 			$Sprite.flip_h = false
-	elif Input.is_action_pressed("ui_left"):
-				velocity.x = max(velocity.x - ACCELERATION, -MAX_SPEED)
-				$Sprite.flip_h = true
-	if Input.is_action_pressed("parry") and Canparry:
-		fire()
-	
-	
+		elif Input.is_action_pressed("ui_left"):
+			velocity.x = max(velocity.x - ACCELERATION, -MAX_SPEED)
+			$Sprite.flip_h = true
+		if Input.is_action_pressed("parry") and Canparry:
+			fire($Position2D.global_position, global_position)
+			rpc("fire", $Position2D.global_position, global_position)
+		rset("puppet_flip_h", $Sprite.flip_h)
+		rset("puppet_position", position)
+	else:
+		$Sprite.flip_h = puppet_flip_h
+		position = puppet_position
