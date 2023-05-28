c6b3ab92d3b83c9fea20e3d42c1f501f8498ab69
Username1unavailable <107366609+Username1unavailable@users.noreply.github.com>
2023-05-27 19:01:53 -0700

    qwd

--- DABULL.gd
+++ DABULL.gd
@@ -2,14 +2,9 @@ extends Area2D
 var direction = Vector2. RIGHT
 var speed = 400
 
-puppet var puppet_position = Vector2()
-
 func _process(delta):
-	if is_network_master():
-		translate(direction.normalized()* speed * delta)
-		rset("puppet_position", position)
-	else:
-		position = puppet_position
+
+	translate(direction.normalized()* speed * delta)
 
 
 func _on_DABULL_body_entered(body):
--- knight.gd
+++ knight.gd
@@ -9,52 +9,46 @@ const RUN_SPEED_THRESHOLD = 0
 const IDLE_THRESHOLD = 10
 var velocity = Vector2(0, 0)
 
-puppet var puppet_position = Vector2()
-
 func _physics_process(delta):
-	if is_network_master():
-		velocity.y += GRAVITY
-		var friction = false
+	velocity.y += GRAVITY
+	var friction = false
 
-		if Input.is_action_pressed("ui_right"):
-			velocity.x = min(velocity.x + ACCELERATION, MAX_SPEED)
-			$Sprite.flip_h = false
-			if Input.is_action_pressed("rightrun") and velocity.x >= RUN_SPEED_THRESHOLD:
-				$AnimationPlayer.play("run")
-				velocity.x += 100
-			else:
-				$AnimationPlayer.play("walk")
-		elif Input.is_action_pressed("ui_left"):
-			velocity.x = max(velocity.x - ACCELERATION, -MAX_SPEED)
-			$Sprite.flip_h = true
-			if Input.is_action_pressed("leftrun") and abs(velocity.x) >= RUN_SPEED_THRESHOLD:
-				$AnimationPlayer.play("run")
-				velocity.x -= 100
-			else:
-				$AnimationPlayer.play("walk")
-		elif Input.is_action_pressed("parry"):
-			$AnimationPlayer.play("attack")
-		elif Input.is_action_pressed("sword"):
-			$AnimationPlayer.play("defend")
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = min(velocity.x + ACCELERATION, MAX_SPEED)
+		$Sprite.flip_h = false
+		if Input.is_action_pressed("rightrun") and velocity.x >= RUN_SPEED_THRESHOLD:
+			$AnimationPlayer.play("run")
+			velocity.x += 100
 		else:
-			friction = true
-			$AnimationPlayer.play("idle")
-
-		if is_on_floor():
-			if Input.is_action_just_pressed("ui_up"):
-				velocity.y = JUMP_HEIGHT
-			if friction == true:
-				velocity.x = lerp(velocity.x, 0, 0.2)
+			$AnimationPlayer.play("walk")
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = max(velocity.x - ACCELERATION, -MAX_SPEED)
+		$Sprite.flip_h = true
+		if Input.is_action_pressed("leftrun") and abs(velocity.x) >= RUN_SPEED_THRESHOLD:
+			$AnimationPlayer.play("run")
+			velocity.x -= 100
 		else:
-			$AnimationPlayer.play("jump")
-			if friction == true:
-				velocity.x = lerp(velocity.x, 0, 0.05)
-
-		velocity = move_and_slide(velocity, UP)
-
-		if Input.is_action_just_pressed("ui_click"):
-			var mousePosition = get_global_mouse_position()
-			global_position = mousePosition
-		rset("puppet_position", position)
+			$AnimationPlayer.play("walk")
+	elif Input.is_action_pressed("parry"):
+		$AnimationPlayer.play("attack")
+	elif Input.is_action_pressed("sword"):
+		$AnimationPlayer.play("defend")
 	else:
-		position = puppet_position
+		friction = true
+		$AnimationPlayer.play("idle")
+
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = JUMP_HEIGHT
+		if friction == true:
+			velocity.x = lerp(velocity.x, 0, 0.2)
+	else:
+		$AnimationPlayer.play("jump")
+		if friction == true:
+			velocity.x = lerp(velocity.x, 0, 0.05)
+
+	velocity = move_and_slide(velocity, UP)
+
+	if Input.is_action_just_pressed("ui_click"):
+		var mousePosition = get_global_mouse_position()
+		global_position = mousePosition
